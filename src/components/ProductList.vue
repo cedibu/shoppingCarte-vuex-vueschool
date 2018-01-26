7a224b675ecfe40c3085c8a5a551c0475f61c7b4
Cedric <cedric.bulloni@gmail.com>
2018-01-26 11:02:44 +0100

    VueSchool dev

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
     <h1>Product List</h1>
-    <ul>
-      <li v-for="product in products">{{product.title}} - {{product.price}}</li>
+      <img
+        v-if="loading"
+        src="https://i.imgur.com/JfPpwOA.gif"
+      >
+    <ul v-else>
+      <li v-for="product in products">
+        {{product.title}} - {{product.price | currency}} - {{product.inventory}}
+        <button @click="addProductToCart(product)">Add to cart</button>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-  import shop from '@/api/shop'
-  import store from '@/store/index'
-
   export default {
+    data () {
+      return {
+        loading: false
+      }
+    },
     computed: {
       products () {
-        return store.state.products
+        return this.$store.getters.availableProducts
+      }
+    },
+    methods: {
+      addProductToCart (product) {
+        this.$store.dispatch('addProductToCart', product)
       }
     },
     created () {
-      shop.getProducts(products => {
-        store.commit('setProducts', products)
-      })
+      this.loading = true
+      this.$store.dispatch('fetchProducts')
+        .then(() => this.loading = false)
     }
   }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,113 @@
 import Vuex from 'vuex'
 import Vue from 'vue'
+import shop from '@/api/shop'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: { // -- data
-    products: []
+  state: { // = data
+    products: [],
+    cart: [],
+    checkoutStatut: null
   },
 
-  getters: { //- computed properties
-    productsCount () {
+  getters: { // = computed properties
+    availableProducts (state, getters) {
+      return state.products.filter(product => product.inventory > 0)
+    },
 
+    cartProducts (state) {
+      return state.cart.map(cartItem => {
+        const product = state.products.find(product => product.id === cartItem.id)
+        return {
+          title: product.title,
+          price: product.price,
+          quantity: cartItem.quantity
+        }
+      })
+    },
+
+    cartTotal(state, getters) {
+      //idem que le reduce ci-dessous
+      // let total = 0
+      // getters.cartProducts.forEach(product => {
+      //   total += product.price * product.quantity
+      // })
+      // return total
+      return getters.cartProducts.reduce((total, product) => total + product.price * product.quantity, 0)
     }
   },
 
-  actions: { //peut être complex mais n'altère pas le state
-    fetchProducts () {
-      //make the call
-      // run setProducts mutation
+  // - L'action décide quand une mutation doit se déclencher
+  // - L'action peut être complex mais n'altère pas le state
+  actions: { // = methods
+    fetchProducts ({commit}) { //destructuring de "context" afin de récupérer juste "commit" et pouvoir appeler commit('...') plus bas au lieu de context.commit
+      return new Promise((resolve, reject) => {
+        //make the call
+        // run setProducts mutation
+        shop.getProducts(products => {
+          commit('setProducts', products)
+          resolve()
+        })
+      })
+    },
+
+    addProductToCart (context, product) {
+      if (product.inventory > 0) {
+        const cartItem = context.state.cart.find(item => item.id === product.id)
+        if (!cartItem) {
+          context.commit('pushProductToCart', product.id)
+        } else {
+          context.commit('incrementItemQuantity', cartItem)
+        }
+        context.commit('decrementProductInventory', product)
+      }
+    },
+
+    checkout ({state, commit}) {
+      shop.buyProducts(
+        state.cart,
+        () => {
+          commit('emptyCart')
+          commit('setCheckoutStatus', 'success')
+        },
+        () => {
+          commit('setCheckoutStatus', 'fail')
+        }
+      )
     }
   },
 
+  //Les mutations sont responsables des changements du state et doivent être simple
   mutations: {
-    //- doit être simple mais altère le state
     //- toujours utiliser les mutations pour updater le state
-    setProducts(state, products) {
+    setProducts (state, products) {
       //Update products
       state.products = products
+    },
+
+    pushProductToCart (state, productId) {
+      state.cart.push({
+        id: productId,
+        quantity: 1
+      })
+    },
+
+    incrementItemQuantity(state, cartItem) {
+      cartItem.quantity++
+    },
+
+    decrementProductInventory(state, product) {
+      product.inventory--
+    },
+
+
+    setCheckoutStatus(state, status) {
+      state.checkoutStatut = status
+    },
+
+    emptyCart(state) {
+      state.cart = []
     }
   }
 })
